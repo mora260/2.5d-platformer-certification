83d9f94b1e47456c2163cd0f68b19f896b3cddaf
Andres Mora <mora1472@gmail.com>
2021-10-10 03:14:50 -0600

    Add left and right movement. Add gravity.

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    float _speed, _gravity;
+    float _yVelocity;
+
+    CharacterController _cc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _cc = GetComponent<CharacterController>();
+
+        if (_cc == null) {
+            Debug.LogError("No character controller found on the player!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateMovement();
+    }
+
+    private void CalculateMovement() {
+        if (_cc.isGrounded) {
+            _yVelocity = 0;
+        }
+
+        float direction = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(0, 0, direction) * _speed;
+        ApplyGravity(ref movement);
+        _cc.Move(movement * Time.deltaTime);
+    }
+
+    private void ApplyGravity(ref Vector3 movement) {
+        if (_cc.isGrounded == false) {
+            _yVelocity+= _gravity * Time.deltaTime;
+            movement.y=_yVelocity;
+        }
     }
 }
