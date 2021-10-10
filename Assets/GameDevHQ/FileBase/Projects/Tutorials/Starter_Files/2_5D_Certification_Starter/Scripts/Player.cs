b18cb21825a126e07cea3d87c2643b9e7a050178
Andres Mora <mora1472@gmail.com>
2021-10-10 03:32:51 -0600

    Add Jump to character controller

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    float _speed, _gravity;
-    float _yVelocity;
+    private float _speed, _gravity, _jumpHeight;
+    private float _yVelocity = 0;
+    private float _recentlyGrounded = 0;
 
     CharacterController _cc;
 
@@ -29,12 +30,22 @@ public class Player : MonoBehaviour
     private void CalculateMovement() {
         if (_cc.isGrounded) {
             _yVelocity = 0;
+            _recentlyGrounded = 0.3f;
         }
 
         float direction = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(0, 0, direction) * _speed;
+
+        if (_recentlyGrounded >= 0 && Input.GetButtonDown("Jump")) {
+            _yVelocity = Mathf.Sqrt(_jumpHeight * -2.0f * _gravity);
+        }
+
         ApplyGravity(ref movement);
         _cc.Move(movement * Time.deltaTime);
+
+        if (_recentlyGrounded > 0) {
+            _recentlyGrounded-=Time.deltaTime;
+        }
     }
 
     private void ApplyGravity(ref Vector3 movement) {
